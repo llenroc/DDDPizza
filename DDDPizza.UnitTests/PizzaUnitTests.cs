9975a501ba20b80f5e263ed4c98b389c57090728
Eric Chavez <echavez@gmail.com>
2015-05-10 22:17:26 -0700

    tested simple unit tests

---
+++ DDDPizza.UnitTests/PizzaUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using DDDPizza.DomainModels;
+using DDDPizza.Mocks;
+using NUnit.Framework;
+
+namespace DDDPizza.UnitTests
+{
+
+    [TestFixture]
+    public class PizzaUnitTests
+    {
+
+        [Test]
+        public void Test()
+        {
+            var sut = new Pizza(PizzaMocks.ToppingMocks().ToList(), PizzaMocks.SizeMocks().First());
+            Assert.IsInstanceOf<Pizza>(sut);
+            Assert.Greater(sut.Total,0);
+        }
+
+    }
+}
